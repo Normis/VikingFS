1adc3dcaf9503c8fa109493408c6c7b63bd6486f
Simon Levesque-Gobeil <simonlg8@gmail.com>
2015-01-28 14:39:14 -0500

    ops..

--- VikingFS/FieldComparer.cs
+++ VikingFS/FieldComparer.cs
@@ -10,18 +10,31 @@ namespace VikingFS
     {
         Dictionary<string, string> comFile;
         Dictionary<string, string> incrFile;
-        string comFilePath, incrFilePath;
-        public FieldComparer(string comFilePath, string incrFilePath)
+        string comFilePath, incrFilePath,
+               comFileName, incrFileName;
+        public FieldComparer(string comFilePath, string comFileName, string incrFilePath, string incrFileName)
         {
             this.comFilePath = comFilePath;
+            this.comFileName = comFileName;
             this.incrFilePath = incrFilePath;
+            this.incrFileName = incrFileName;
 
         }
 
-        public void commit(Dictionary<string, string> repository)
+        public void commit(long revision = -1)
         {
-            fileMiddleware fm = new fileMiddleware(incrFilePath);
-            Dictionary<string, string> result = comFile.Keys.Intersect(incrFile.Keys).ToDictionary(t => t, comFile.Keys[t]);
+            IncrementalFileSystem ifs = new IncrementalFileSystem(incrFilePath,incrFileName);
+            incrFile = ifs.GetValues(revision);
+            Dictionary<string, string> result = comFile.Keys.Intersect(incrFile.Keys).ToDictionary(t => t,t => comFile[t]);
+            Dictionary<string, string> result1 = incrFile.Keys.Intersect(comFile.Keys).ToDictionary(t => t, t => incrFile[t]);
+            foreach(KeyValuePair<string,string> pair in incrFile)
+            {
+                string value = comFile[pair.Key];
+                if(comFile[pair.Key] != null)
+                {
+
+                }
+            }
             //..
         }
 
