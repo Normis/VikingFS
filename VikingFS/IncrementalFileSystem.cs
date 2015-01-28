5a4f29277ada02a22a4e92e539225b21c47c6318
Frank Gen <frankgenerated@gmail.com>
2015-01-28 01:24:33 -0500

    Tested

--- VikingFS/IncrementalFileSystem.cs
+++ VikingFS/IncrementalFileSystem.cs
@@ -10,11 +10,11 @@ namespace VikingFS
     {
         private string filepath;
         private StringBuilder modifs;
-        public IncrementalFileSystem(string filePath)
+        public IncrementalFileSystem(string _filePath)
         {
-            if (!System.IO.File.Exists(filepath))
-                System.IO.File.WriteAllText(filepath, "0\n");
-            this.filepath = filepath;
+            if (!System.IO.File.Exists(_filePath))
+                System.IO.File.WriteAllLines(_filePath, new string[] { "0" });
+            this.filepath = _filePath;
             modifs = new StringBuilder();
         }
 
@@ -31,12 +31,33 @@ namespace VikingFS
         public void Commit()
         {
             long lastLine = Convert.ToInt64(System.IO.File.ReadLines(this.filepath).Last());
-            using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\Public\TestFolder\WriteLines2.txt"))
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(this.filepath, true))
             {
                 file.Write(modifs.ToString());
                 modifs.Clear();
-                file.WriteLine(lastLine.ToString());
-            }   
+                file.WriteLine((lastLine + 1).ToString());
+            }
+        }
+
+        public Dictionary<string, string> GetValues()
+        {
+            Dictionary<string, string> dict = new Dictionary<string,string>();
+            using (System.IO.StreamReader file = new System.IO.StreamReader(this.filepath))
+            {
+                while (true)
+                {
+                    string line = file.ReadLine();
+                    if (line == null)
+                        break;
+                    
+                    try { Convert.ToInt64(line); }
+                    catch(Exception e) {
+                        string[] liste = line.Split('=');
+                        dict[liste[0]] = liste[1];
+                    }
+                }
+            }
+            return dict;
         }
     }
 }
--- VikingFS/Program.cs
+++ VikingFS/Program.cs
@@ -8,8 +8,27 @@ namespace VikingFS
 {
     class Program
     {
+        public static void Tests()
+        {
+            var fs = new IncrementalFileSystem(@"C:\Users\Utilisateur\prog\test\truc.txt");
+            fs.AddChange("IDENT.Ident4", "Banane");
+            fs.AddChange("IDENT.Ident400", "135");
+            fs.Commit();
+            fs.AddChange("IDENT.Ident348", "Boom");
+            fs.AddChange("IDENT.Ident349", "Chackalaka");
+            fs.Commit();
+
+            var dict = fs.GetValues();
+
+            foreach(var banane in dict)
+                Console.WriteLine(banane.Key + " " + banane.Value);
+
+            Console.ReadKey();
+        }
+
         static void Main(string[] args)
         {
+            Tests();
             TaxprepT2Com2014V2.Taxprep2014T2Return taxreturn = new TaxprepT2Com2014V2.Taxprep2014T2Return();
             //taxreturn.Open()
         }
