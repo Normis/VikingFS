a83e5e50192c8fa1f9384f2e5c458ebf96d537e4
yonguelink <isaac.computing@gmail.com>
2015-01-28 03:19:25 -0500

    Some comment

--- VikingFS/Program.cs
+++ VikingFS/Program.cs
@@ -12,6 +12,13 @@ namespace VikingFS
         {
             TaxprepT2Com2014V2.Taxprep2014T2Return taxreturn = new TaxprepT2Com2014V2.Taxprep2014T2Return();
             //taxreturn.Open()
+            /*Console.Write("Enter the middleware's location : ");
+            string middlewareFile = Console.ReadLine();
+            Console.Write("Enter the data location : ");
+            string dataFile = Console.ReadLine();
+            var middleware = new fileMiddleware(middlewareFile);
+            middleware.addNewData(middlewareFile);
+            middleware.commit();*/
         }
     }
 }
