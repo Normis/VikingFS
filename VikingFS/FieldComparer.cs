bdf4a26fbebe6c8171b9f76ed751e295afab98ba
Simon Levesque-Gobeil <simonlg8@gmail.com>
2015-01-28 13:35:15 -0500

    fieldComparer initialized...

---
+++ VikingFS/FieldComparer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VikingFS
+{
+    class FieldComparer
+    {
+        Dictionary<string, string> comFile;
+        Dictionary<string, string> incrFile;
+        string comFilePath, incrFilePath;
+        public FieldComparer(string comFilePath, string incrFilePath)
+        {
+            this.comFilePath = comFilePath;
+            this.incrFilePath = incrFilePath;
+
+        }
+
+        public void commit(Dictionary<string, string> repository)
+        {
+            fileMiddleware fm = new fileMiddleware(incrFilePath);
+            Dictionary<string, string> result = comFile.Keys.Intersect(incrFile.Keys).ToDictionary(t => t, comFile.Keys[t]);
+            //..
+        }
+
+    }
+}
