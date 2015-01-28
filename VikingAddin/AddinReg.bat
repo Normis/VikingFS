db5e6f74578ec3d5bf4db7340e539d95fb473ef8
Frank Gen <frankgenerated@gmail.com>
2015-01-28 09:36:15 -0500

    whatveer

--- VikingAddin/AddinReg.bat
+++ VikingAddin/AddinReg.bat
@@ -8,10 +8,8 @@ set AddinVersion=1.0.0.0
 set AddinDLL=%~dp0bin\Debug\EmptyAddin.dll
 
 set directory=%~dp0
-set exe=%directory%\..\..\api\AddinReg.exe
-set proxy=%~dp0Proxy\VsProxy.dll
-set T1=T1 Taxprep 2014
+set exe=.\api\AddinReg.exe
+set proxy=%~dp0bin\Debug\VsProxy.dll
 set T2=T2 Taxprep 2014-2
 
-"%exe%" "%T1%" -register -p "%AddinShortName%" "%AddinName%" "%AddinGuid%" "%AddinVersion%" "%AddinDLL%" "%proxy%"
 "%exe%" "%T2%" -register -p "%AddinShortName%" "%AddinName%" "%AddinGuid%" "%AddinVersion%" "%AddinDLL%" "%proxy%"
